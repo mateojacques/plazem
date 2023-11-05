53c98578b4a4eb0b5d75c27655e61df428604b93
Mateo Jacques <mateojacquesweb@gmail.com>
2023-11-05 12:27:09 -0300

    Fix header on mobile devices and disable scroll when a game has already started

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,27 @@ import LanguageSelection from "./components/language/LanguageSelection";
 import Table from "./components/table/Table";
 import { BODY_CLASSNAME } from "./utils/constants";
 import Header from "./components/header/Header";
+import { useContext, useEffect } from "react";
+import { TableContext } from "./contexts/tableContext";
 
 function App() {
-  if (window.matchMedia("(min-width: 768px)").matches) {
-    window.onscroll = function () {
-      window.scrollTo(0, 0);
-    };
-  }
+  const { cardAmount } = useContext(TableContext);
+
+  useEffect(() => {
+    const gameStarted = Object.values(cardAmount).reduce(
+      (result, value) => result || value > 0,
+      false
+    );
+    if (window.matchMedia("(min-height: 800px)").matches || gameStarted) {
+      const scrollX = window.scrollX;
+      const scrollY = window.scrollY;
+      window.onscroll = function () {
+        window.scrollTo(scrollX, scrollY);
+      };
+    } else {
+      window.onscroll = function () {};
+    }
+  }, [cardAmount]);
 
   return (
     <main className={BODY_CLASSNAME}>
